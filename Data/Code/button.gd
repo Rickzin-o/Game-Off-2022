e59c00dbce216e955844141bfb683ddc5ba2e2e7
José Ricardo <ricardoanimation2@gmail.com>
2022-11-23 22:07:55 -0300

    Green Button art

--- Data/Code/button.gd
+++ Data/Code/button.gd
@@ -3,5 +3,7 @@ extends TextureButton
 export(String) var nextScene = 'menuprincipal'
 
 func _on_Button_pressed():
+	get_child(0).rect_position.y += 2
 	SoundManager.play_ui_sound(load("res://Data/Sounds/SFX/SFXBlip.wav"))
+	yield(get_tree().create_timer(0.1), "timeout")
 	get_tree().change_scene("res://Data/Scenes/" + nextScene + ".tscn")
