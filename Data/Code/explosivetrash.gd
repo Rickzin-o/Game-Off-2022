4a83935c08c0d6b974e2403804277a1630deb5e6
José Ricardo <ricardoanimation2@gmail.com>
2022-11-27 01:18:27 -0300

    Create Explosive Trash
    Explodes when you interact with it

---
+++ Data/Code/explosivetrash.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var playerinside = false
+
+
+func _input(event):
+	if Input.is_action_just_pressed("interact"):
+		if playerinside:
+			$AnimationPlayer.play("explode")
+
+
+func _on_Area2D_body_entered(body):
+	playerinside = true
+
+
+func _on_Area2D_body_exited(body):
+	playerinside = false
+
+
+func _on_Explosion_Hitbox_body_entered(body):
+	if body is Player:
+		if not body.intangible:
+			body.take_damage(5, 25)
--- Data/Code/spike.gd
+++ Data/Code/spike.gd
@@ -4,4 +4,4 @@ extends Node2D
 
 func _on_Area2D_body_entered(body: Player):
 	if not body.intangible:
-		body.take_damage(3, 15)
+		body.take_damage(4, 20)
